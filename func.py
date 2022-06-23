bd881ab0a9e2435f15accb6f077d5bdbfa05f20c
NextFutureOfficial <61616918+NextFutureOfficial@users.noreply.github.com>
2022-06-23 11:53:22 +0000

    update

---
+++ func.py
@@ -0,0 +1,76 @@
+import time
+import subprocess
+
+
+
+#基础函数，用于在shell中执行命令并返回shell的打印结果
+def RunInShell(comm):
+    subp=subprocess.Popen(comm,shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE,encoding="utf-8")
+    return_text=subp.communicate()[0]
+    if return_text is None:
+        return_text='Error: Program has return None'
+    return return_text
+
+#从top指令中提取CPU相关信息
+def CPUUsage():
+    top_text=RunInShell('top -bn 1 -i -c')
+    top_text_lines=top_text.splitlines()
+    cpu_usage_line=top_text_lines[2]
+    cpu_usage_info_list=cpu_usage_line.split(',  ')
+    cpu_usage=cpu_usage_info_list[0]
+    cpu_usage=cpu_usage.replace('%Cpu(s):','')
+    cpu_usage=cpu_usage.replace(' ','')
+    cpu_usage=cpu_usage.replace('us','')
+    print('cpu_usage',cpu_usage)
+    return str(cpu_usage)
+
+#task运行情况
+def TaskUsageTotal():
+    top_text=RunInShell('top -bn 1 -i -c')
+    top_text_lines=top_text.splitlines()
+    task_usage_line=top_text_lines[1]
+    task_usage_line=task_usage_line.replace(' ','')
+    task_usage_info_list=task_usage_line.split(',')
+    task_usage_info_total=task_usage_info_list[0] #处理总进程数量
+    task_usage_info_total=task_usage_info_total.replace('Tasks:','')
+    task_usage_info_total=task_usage_info_total.replace('total','')
+    print('task total:',task_usage_info_total)
+    return str(task_usage_info_total)
+
+#task运行情况，正在运行
+def TaskUsageNow():
+    top_text=RunInShell('top -bn 1 -i -c')
+    top_text_lines=top_text.splitlines()
+    task_usage_line=top_text_lines[1]
+    task_usage_line=task_usage_line.replace(' ','')
+    task_usage_info_list=task_usage_line.split(',')
+    task_usage_info_now=task_usage_info_list[1] #处理正在运行进程数量
+    task_usage_info_now=task_usage_info_now.replace(' ','')
+    task_usage_info_now=task_usage_info_now.replace('running','')
+    print('task now:',task_usage_info_now)
+    return str(task_usage_info_now)
+
+#内存使用状况
+def CacheUsage():
+    #获取基础的行，删除空格，并基于逗号对行进行切分
+    top_text=RunInShell('top -bn 1 -i -c')
+    top_text_lines=top_text.splitlines()
+    mem_usage_line=top_text_lines[3]
+    mem_usage_line=mem_usage_line.replace(' ','')
+    mem_usage_info_list=mem_usage_line.split(',')
+    #处理内存数据，总内存
+    mem_usage_info_total=mem_usage_info_list[0]
+    mem_usage_info_total=mem_usage_info_total.replace(' ','')
+    mem_usage_info_total=mem_usage_info_total.replace('MiBMen:','')
+    mem_usage_info_total=mem_usage_info_total.replace('total:','')
+    #处理可用内存
+    mem_usage_info_free=mem_usage_info_list[1]
+    mem_usage_info_free=mem_usage_info_free.replace(' ','')
+    mem_usage_info_free=mem_usage_info_free.replace('free','')
+    #计算使用百分比
+    mem_usage_info_used=mem_usage_info_total-mem_usage_info_free
+    percent=(mem_usage_info_used/mem_usage_info_total)*100
+    percent=str(percent)+'%'
+    #构造返回数列
+    return_list=[mem_usage_info_total,mem_usage_info_free,percent]
+    return return_list
--- main.py
+++ main.py
@@ -2,22 +2,30 @@ import subprocess
 import time
 import telebot
 
-
-#编写基础函数
-def RunInShell(comm):
-    subp=subprocess.Popen(comm,shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE,encoding="utf-8")
-    return_text=subp.communicate()[0]
-    if return_text is None:
-        return_text='Error: Program has return None'
-    return return_text
-
-#开始构造返回信息
-info_text=''
-info_text+='<strong>HostName:</strong> '+RunInShell('hostname') #vps hostname
-info_text+='<strong>CPU Usage:</strong> '+RunInShell('top -bn 1 -i -c')+'\n' #vps hostname
+from func import RunInShell
+from func import CPUUsage
+from func import TaskUsageTotal
+from func import TaskUsageNow
+from func import CacheUsage
 
 
 
-#使用Bot发送生成的信息
-bot = telebot.TeleBot("5050467713:AAFQultsmN1YdFnp9MVZiBakXSEXVr1ED6M", parse_mode='HTML')
-bot.send_message(975140440,info_text)
+while(True):
+    #开始构造返回信息
+    info_text=''
+    info_text+='<strong>HostName:</strong> '+RunInShell('hostname') #vps hostname
+    info_text+='<strong>CPU Usage:</strong> '+CPUUsage()+'\n' #cpu usage
+    #处理内存使用
+    mem_info=CacheUsage()
+    mem_total=mem_info[0]
+    mem_free=mem_info[1]
+    mem_percent=mem_info[2]
+    info_text+='<strong>Memory Usage:</strong> '+CPUUsage()+'\n' #cpu usage
+    info_text+='<strong>Task Usage:</strong> '+'总任务数：'+TaskUsageTotal()+'  正在运行：'+TaskUsageNow()+'\n' #task usage
+
+
+
+    #使用Bot发送生成的信息
+    bot = telebot.TeleBot("5050467713:AAFQultsmN1YdFnp9MVZiBakXSEXVr1ED6M", parse_mode='HTML')
+    bot.send_message(975140440,info_text)
+    time.sleep(3)
