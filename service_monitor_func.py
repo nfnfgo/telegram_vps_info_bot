58c3ed2005d71e1ad58938a8f8f5243cb46fd069
NextFutureOfficial <61616918+NextFutureOfficial@users.noreply.github.com>
2022-07-02 03:35:16 +0000

    update

--- service_monitor_func.py
+++ service_monitor_func.py
@@ -20,10 +20,21 @@ def ServStat():
 
     # get service status text in by using shell command
     serv_res_text=RunInShell('service --status-all')
+    res_text_lines=serv_res_text.split('\n')
+
+    # Initialize return text here for later ref
+    res_text='<strong>服务运行状态: </strong>\n'
 
     # Use the  shell respone to check the service status
-    for serv_item in serv_dict.keys():
+    for serv_item in serv_dict.keys(): # try to check every single item by iterate through the list
         check_list=serv_dict[serv_item]
-        print(check_list)
+        for line in res_text_lines: # iterate every single lines, try to find the corresponding line
+            times=0 # set a flag variable, if it equals element nums in check_list then pass
+            for text in check_list:
+                if text in line:
+                    times+=1
+            if 
+
+        
 
     return serv_res_text
