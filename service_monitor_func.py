fa56fdb7601d994d1b6f7bbaa27ebb78a55551c2
NextFutureOfficial <61616918+NextFutureOfficial@users.noreply.github.com>
2022-07-02 03:47:54 +0000

    update

--- service_monitor_func.py
+++ service_monitor_func.py
@@ -23,18 +23,26 @@ def ServStat():
     res_text_lines=serv_res_text.split('\n')
 
     # Initialize return text here for later ref
-    res_text='<strong>服务运行状态: </strong>\n'
+    res_text='<strong>VPS Service Status: </strong>\n'
 
     # Use the  shell respone to check the service status
     for serv_item in serv_dict.keys(): # try to check every single item by iterate through the list
         check_list=serv_dict[serv_item]
+        line_times=0
         for line in res_text_lines: # iterate every single lines, try to find the corresponding line
-            times=0 # set a flag variable, if it equals element nums in check_list then pass
+            line_times+=1
+            check_times=0 # set a flag variable, if it equals element nums in check_list then pass
             for text in check_list:
                 if text in line:
-                    times+=1
-            if 
+                    check_times+=1
+            if check_times==len(check_list):
+                res_text+=serv_item+': Running\n'
+                break
+            elif line_times<len(res_text_lines):
+                pass
+            else:
+                res_text+=serv_item+': <strong>Error Occurred!</strong>\n'
 
         
 
-    return serv_res_text
+    return res_text
